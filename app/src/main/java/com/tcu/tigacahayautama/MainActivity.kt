4ff4ac367cd63531cff71e6ba8c696f8eb09fb56
Muhajir <xshitcode@gmail.com>
2024-05-24 14:22:22 +0700

    feat: webview

---
+++ app/src/main/java/com/tcu/tigacahayautama/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.tcu.tigacahayautama
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl("app.tigacahayautama.com")
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webViewClient = WebViewClient();
+        myWebView.settings.setSupportZoom(true)
+    }
+}
